f1170c51ddb6c9be4fd2b902d75a2e8547b0f489
Grid03 <gamer2003rayas@gmail.com>
2023-04-18 09:01:08 +0200

    Update MainPokemon.java

--- ProyectoPokemon/src/pokemon/MainPokemon.java
+++ ProyectoPokemon/src/pokemon/MainPokemon.java
@@ -1,7 +1,34 @@
 package pokemon;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainPokemon {
 	public static void main(String[] args) {
-		System.out.println("hola");
+
+		Connection connection = null;
+		String url = "jdbc:mysql://localhost:3306/pokemon";
+		String login = "root";
+		String password = "";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url, login, password);
+			System.out.println("Conexión establecida");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+				System.out.println("Conexión cerrada");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
